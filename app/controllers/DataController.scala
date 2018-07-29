4c1f0ea98ff93b0636da7ded7b5c36001fe9c3a1
Stanislav Bytsko <stanislav.bytsko@nextiva.com>
2018-07-29 14:44:52 +0300

    Remove dummy create endpoint

--- app/controllers/DataController.scala
+++ app/controllers/DataController.scala
@@ -1,8 +1,7 @@
 package controllers
 
-import dto.DocDto
 import javax.inject.{Inject, Singleton}
-import play.api.libs.json.{JsPath, Json, JsonValidationError}
+import play.api.libs.json.Json
 import play.api.mvc._
 import services.ElasticService
 
@@ -12,22 +11,12 @@ import scala.concurrent.ExecutionContext
 class DataController @Inject()(val cc: ControllerComponents,
                                val elastic: ElasticService)(implicit val ec: ExecutionContext) extends AbstractController(cc) {
 
-  def create: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    val dto: DocDto = Json.fromJson[DocDto](request.body.asJson.get).asEither match {
-      case Right(value) => value
-      case Left(errs: Seq[(JsPath, Seq[JsonValidationError])]) =>
-        throw new IllegalArgumentException(s"Bad request: ${errs.last}")
-    }
-
-    elastic.send(dto).map(ignored => Created)
-  }
-
   def search: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    val namePattern: String = request.getQueryString("name") match {
+    val pattern: String = request.getQueryString("name") match {
       case Some(name) => name
       case None => throw new IllegalArgumentException(s"missing required query parameter 'name', got ${request.rawQueryString}")
     }
-    elastic.search(namePattern)
+    elastic.search(pattern)
       .map(res => Ok(Json.parse(res.body)))
   }
 }
--- app/services/ElasticService.scala
+++ app/services/ElasticService.scala
@@ -2,7 +2,6 @@ package services
 
 import dto.DocDto
 import javax.inject.{Inject, Singleton}
-import model.Doc
 import play.api.http.HttpVerbs
 import play.api.libs.json.Json
 import play.api.libs.ws.{WSClient, WSResponse}
@@ -12,20 +11,23 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class ElasticService @Inject()(val ws: WSClient)(implicit val ec: ExecutionContext) {
 
+  // we are using one mapping and data type per instance of app
   private val esDocRoot: String = "http://localhost:9200/docs/_doc"
 
-  def send(enrichedWithId: DocDto): Future[WSResponse] = {
-    ws.url(esDocRoot).post(Json.toJson(enrichedWithId))
+  def send(docDto: DocDto): Future[WSResponse] = {
+    ws.url(esDocRoot).post(
+      Json.toJson(docDto)
+    )
   }
 
-  def search(namePattern: String): Future[WSResponse] = {
+  def search(pattern: String): Future[WSResponse] = {
     ws.url(s"$esDocRoot/_search")
       .withMethod(HttpVerbs.GET)
       .withBody(Json.parse(
         s"""
            |{
            |    "query" : {
-           |        "term" : { "name" : "$namePattern" }
+           |        "term" : { "name" : "$pattern" }
            |    }
            |}
         """.stripMargin))
